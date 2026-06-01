c6d1afff6bf06649f62de813c183444b602c17cb
Nellyta <nnarou@gmail.com>
2026-06-01 18:11:16 +0200

    J0 - initialisation repo + structure projet + datasets Silver

--- old/Jedha Exo/player.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    """Represents a player with name and symbol."""
-    
-    def __init__(self, name, symbol):
-        self.name = name
-        self.symbol = symbol
-    
-    def __str__(self):
-        return f"{self.name} ({self.symbol})"
